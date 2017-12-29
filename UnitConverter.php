93d157d2136a9e9e02872708b3850593b89bb6a7
arwalton <adamrwalton@gmail.com>
2017-12-29 16:05:05 +0200

    This is my inital setup for the calculateUnits project

---
+++ UnitConverter.php
@@ -0,0 +1,110 @@
+<html>
+ <head>
+  <title>Awesome Unit Converter</title>
+ </head>
+ <body>
+ <form action = "calculateUnits.php" method = "get">
+  <p>
+  What unit are you starting with?
+  <select name = "fromUnit">
+	<option value = "">Select...</option>
+    <option value = "Attometer">Attometer</option>
+	<option value = "Barleycorn">Barleycorn</option>
+	<option value = "Centimeter">Centimeter</option>
+	<option value = "Cubit">Cubit</option>
+	<option value = "Decameter">Decameter (Dekameter)</option>
+	<option value = "Decimeter">Decimeter</option>
+	<option value = "Exameter">Exameter</option>
+	<option value = "Femtometer">Femtometer</option>
+	<option value = "Foot">Foot</option>
+	<option value = "Furlong">Furlong</option>
+	<option value = "Gigameter">Gigameter</option>
+	<option value = "Hand">Hand</option>
+	<option value = "Hectometer">Hectometer</option>
+	<option value = "Horse Length">Horse Length</option>
+	<option value = "Inch">Inch</option>
+	<option value = "Kilometer">Kilometer</option>
+	<option value = "League">League</option>
+	<option value = "Light-nanosecond">Light-nanosecond</option>
+	<option value = "Light-year">Light-year</option>
+	<option value = "Lunar Distance">Lunar Distance</option>
+	<option value = "Megameter">Megameter</option>
+	<option value = "Meter">Meter</option>
+	<option value = "Metric Foot">Metric Foot</option>
+	<option value = "Micrometer">Micrometer</option>
+	<option value = "Mile">Mile</option>
+	<option value = "Millimeter">Millimeter</option>
+	<option value = "Nanometer">Nanometer</option>
+	<option value = "Nautical Mile">Nautical Mile</option>
+	<option value = "Parsec">Parsec</option>
+	<option value = "Peninkulma">Peninkulma</option>
+	<option value = "Petameter">Petameter</option>
+	<option value = "Picometer">Picometer</option>
+	<option value = "Poronkusema">Poronkusema</option>
+	<option value = "Rack Unit">Rack Unit</option>
+	<option value = "Siriometer">Siriometer</option>
+	<option value = "Smoot">Smoot</option>
+	<option value = "Terameter">Terameter</option>
+	<option value = "Verst">Verst(Finnish)</option>
+	<option value = "Versta">Versta(Russian)</option>
+	<option value = "Yard">Yard</option>
+	<option value = "Yoctometer">Yoctometer</option>
+	<option value = "Yottameter">Yottameter</option>
+	<option value = "Zeptometer">Zeptometer</option>
+	<option value = "Zettameter">Zettameter</option>
+  </select>
+  </p>
+  <p>
+  What unit are you going to?
+  <select name = "toUnit">
+	<option value = "">Select...</option>
+	<option value = "Attometer">Select...</option>
+	<option value = "Barleycorn">Barleycorn</option>
+	<option value = "Centimeter">Centimeter</option>
+	<option value = "Cubit">Cubit</option>
+	<option value = "Decameter">Decameter (Dekameter)</option>
+	<option value = "Decimeter">Decimeter</option>
+	<option value = "Exameter">Exameter</option>
+	<option value = "Femtometer">Femtometer</option>
+	<option value = "Foot">Foot</option>
+	<option value = "Furlong">Furlong</option>
+	<option value = "Gigameter">Gigameter</option>
+	<option value = "Hand">Hand</option>
+	<option value = "Hectometer">Hectometer</option>
+	<option value = "Horse Length">Horse Length</option>
+	<option value = "Inch">Inch</option>
+	<option value = "Kilometer">Kilometer</option>
+	<option value = "League">League</option>
+	<option value = "Light-nanosecond">Light-nanosecond</option>
+	<option value = "Light-year">Light-year</option>
+	<option value = "Lunar Distance">Lunar Distance</option>
+	<option value = "Megameter">Megameter</option>
+	<option value = "Meter">Meter</option>
+	<option value = "Metric Foot">Metric Foot</option>
+	<option value = "Micrometer">Micrometer</option>
+	<option value = "Mile">Mile</option>
+	<option value = "Millimeter">Millimeter</option>
+	<option value = "Nanometer">Nanometer</option>
+	<option value = "Nautical Mile">Nautical Mile</option>
+	<option value = "Parsec">Parsec</option>
+	<option value = "Peninkulma">Peninkulma</option>
+	<option value = "Petameter">Petameter</option>
+	<option value = "Picometer">Picometer</option>
+	<option value = "Poronkusema">Poronkusema</option>
+	<option value = "Rack Unit">Rack Unit</option>
+	<option value = "Siriometer">Siriometer</option>
+	<option value = "Smoot">Smoot</option>
+	<option value = "Terameter">Terameter</option>
+	<option value = "Verst">Verst(Finnish)</option>
+	<option value = "Versta">Versta(Russian)</option>
+	<option value = "Yard">Yard</option>
+	<option value = "Yoctometer">Yoctometer</option>
+	<option value = "Yottameter">Yottameter</option>
+	<option value = "Zeptometer">Zeptometer</option>
+	<option value = "Zettameter">Zettameter</option>
+  </select>
+  </p>
+  <p><input type = "submit" /></p>
+ </form>
+ </body>
+</html>
---
+++ calculateUnits.php
@@ -0,0 +1,9 @@
+<!--
+method = "get">
+
+fromUnit
+toUnit
+
+-->
+
+You chose <?php echo htmlspecialchars($_GET['fromUnit'])?> and <?php echo htmlspecialchars($_GET['toUnit'])?>.
